aad8e3da061b9d658fb935e9c6534b6dbca0b3cb
François Lépine <francois.lepine@gmail.com>
2019-04-07 21:34:45 -0400

    Règle à implémenter.

---
+++ BowlingClasses.Core/ServiceCalculScore.cs
@@ -0,0 +1,21 @@
+﻿using BowlingClasses.Core.Interfaces;
+
+namespace BowlingClasses.Core
+{
+    /// <summary>
+    /// Service pour calculer le score total.
+    /// </summary>
+    public class ServiceCalculScore : IServiceCalculScore
+    {
+        /// <summary>
+        /// Permet de calculer le score final jusqu'à la case courante.
+        /// </summary>
+        /// <param name="lancers">Tableau de toutes les quilles abattues.</param>
+        /// <param name="noCaseCourante">N° de case. Par défaut 10, calculer le tout.</param>
+        /// <returns>Score.</returns>
+        public int Calculer(int[] lancers, int noCaseCourante = 10)
+        {
+            return 0;
+        }
+    }
+}
